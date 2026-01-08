5fd830dcdce9ad8f89e049f6503e0d6747082dcd
Adelina Zivanna <adelinazivanna@gmail.com>
2026-01-08 11:07:42 +0700

    first commit

--- inc/header.php
+++ inc/header.php
@@ -4,26 +4,47 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo isset($pageTitle) ? $pageTitle . ' - ' : ''; ?>Webschool</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"><link rel="stylesheet" href="assets/css/style.css">    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body>
-    <nav class="navbar">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light border">
         <div class="container">
-            <div class="nav-wrapper">
-                <div class="logo">
-                    <i class="fas fa-graduation-cap"></i>
-                    <span>Webschool</span>
-                </div>
-                <ul class="nav-menu">
-                    <li><a href="index.php">Beranda</a></li>
-                    <li><a href="article.php" class="active">Artikel</a></li>
-                    <li><a href="contact-us.php">Kontak</a></li>
+            <div class="logo">
+                <i class="fas fa-graduation-cap"></i>
+                <span>Webschool</span>
+            </div>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item"><a class="nav-link text-dark active" aria-current="page" href="#">Home</a></li>
+                    <li class="nav-item"><a class="nav-link text-dark" href="#">PPDB</a></li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link  text-dark dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Tentang Kami</a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Profil Sekolah</a></li>
+                            <li><a class="dropdown-item" href="#">Fasilitas</a></li>
+                            <li><a class="dropdown-item" href="#">Tenaga Pendidik</a></li>
+                            <li><a class="dropdown-item" href="#">Ekstrakurikuler</a></li>
+                            <li><a class="dropdown-item" href="#">Daftar Prestasi</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link  text-dark dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Blog</a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Kegiatan Sekolah</a></li>
+                            <li><a class="dropdown-item" href="#">Kegiatan Wali Murid</a></li>
+                            <li><a class="dropdown-item" href="#">Pengumuman</a></li>
+                            <li><a class="dropdown-item" href="#">Update Prestasi</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item"><a class="nav-link text-dark" href="#">Kontak Kami</a></li>
                 </ul>
-                <div class="hamburger">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </div>
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
             </div>
         </div>
     </nav>
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 $pageTitle = "Beranda";
+include 'inc/header.php';
 include 'inc/functions.php';
 
 ?>
@@ -14,84 +15,31 @@ include 'inc/functions.php';
     <title>Template Website Sekolah - Naevaweb.com</title>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light border">
-        <div class="container">
-            <a class="navbar-brand" href="#"><img src="https://naevaweb.com/userfiles/uploads/naevaschool-2.svg" height="30" alt="NaevaScool" /></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item"><a class="nav-link text-dark active" aria-current="page" href="#">Home</a></li>
-                    <li class="nav-item"><a class="nav-link text-dark" href="#">PPDB</a></li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link  text-dark dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Tentang Kami</a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Profil Sekolah</a></li>
-                            <li><a class="dropdown-item" href="#">Fasilitas</a></li>
-                            <li><a class="dropdown-item" href="#">Tenaga Pendidik</a></li>
-                            <li><a class="dropdown-item" href="#">Ekstrakurikuler</a></li>
-                            <li><a class="dropdown-item" href="#">Daftar Prestasi</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link  text-dark dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Blog</a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Kegiatan Sekolah</a></li>
-                            <li><a class="dropdown-item" href="#">Kegiatan Wali Murid</a></li>
-                            <li><a class="dropdown-item" href="#">Pengumuman</a></li>
-                            <li><a class="dropdown-item" href="#">Update Prestasi</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item"><a class="nav-link text-dark" href="#">Kontak Kami</a></li>
-                </ul>
-                <form class="d-flex">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
-            </div>
-        </div>
-    </nav>
-    <div id="carouselDekstop" class="carousel slide d-none d-lg-block" data-bs-ride="carousel">
+        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselDekstop" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselDekstop" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselDekstop" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"></button>
         </div>
+
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img src="assets/image/gambar1.jpg" class="d-block w-100" heigth="50" alt="...">
+            <img src="assets/image/gambar1.jpg" class="d-block w-100" alt="Slide 1">
             </div>
             <div class="carousel-item">
-                <img src="assets/image/gambar2.jpg" class="d-block w-100" alt="...">
+            <img src="assets/image/gambar2.jpg" class="d-block w-100" alt="Slide 2">
             </div>
             <div class="carousel-item">
-                <img src="assets/image/gambar3.jpg" class="d-block w-100" alt="...">
+            <img src="assets/image/gambar3.jpg" class="d-block w-100" alt="Slide 3">
             </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselDekstop" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
+
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon"></span>
         </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselDekstop" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span class="carousel-control-next-icon"></span>
         </button>
-    </div>
-    <div id="carouselMobile" class="carousel slide d-lg-none" data-bs-ride="carousel">
-        <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselMobile" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselMobile" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselMobile" data-bs-slide-to="2" aria-label="Slide 3"></button>
-        </div>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="https://naevaschool.naevaweb.my.id/userfiles/uploads/img-edukasi-5-4.jpg" class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
-                <img src="https://naevaschool.naevaweb.my.id/userfiles/uploads/img-edukasi-5-4.jpg" class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
-                <img src="https://naevaschool.naevaweb.my.id/userfiles/uploads/img-edukasi-57.jpg" class="d-block w-100" alt="...">
-            </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselMobile" data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
